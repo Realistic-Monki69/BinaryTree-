8defcfd180092f2f60255bbecd6780c292e71d4c
Hrithik Turaka <hturaka@cherrycreekschools.org>
2024-11-26 17:56:22 +0000

    done

--- TreeNode.java
+++ TreeNode.java
@@ -1,15 +1,47 @@
 public class TreeNode implements Treeable{
-    private Compareable treeNodeValue;
-    private TreeNode leftTreeValue;
-    private TreeNode rightTreeValue;
+    private Comparable treeNodeValue;
+    private TreeNode leftTreeNode;
+    private TreeNode rightTreeNode;
 
-    public Treenode(){
+    public TreeNode(){
         treeNodeValue = null;
-        leftTreeValue = null;
-        rightTreeValue = null;
+        leftTreeNode = null;
+        rightTreeNode = null;
     }
 
-    public Treenode(Comapreable value){
-        
+    public TreeNode(Comparable value){
+        treeNodeValue = value;
+        leftTreeNode = null;
+        rightTreeNode = null;
+    }
+
+    public TreeNode(Comparable value, TreeNode left, TreeNode right){
+        treeNodeValue = value;
+        leftTreeNode = left;
+        rightTreeNode = right;
+    }
+
+    public Comparable getValue(){
+        return treeNodeValue;
+    }
+
+    public TreeNode getLeft(){
+        return leftTreeNode;
+    }
+
+    public TreeNode getRight(){
+        return rightTreeNode;
+    }
+
+    public void setValue(Comparable value){
+        treeNodeValue = value;
+    }
+
+    public void setLeft(Treeable left){
+        leftTreeNode = (TreeNode)left;
+    }
+
+    public void setRight(Treeable right){
+        rightTreeNode = (TreeNode)right;
     }
 }
